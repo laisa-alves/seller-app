94f9dfccfe3c78e69495dcc26882bbcd5cf312d9
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-21 07:57:18 -0300

    Adiciona esqueleto inicial: Homepage e navegação

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheHeading from '@/components/TheHeading.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <!-- Navbar -->
+  <header class="bg-white fixed top-0 left-0 right-0 z-10 drop-shadow-sm">
+    <TheHeading />
+  </header>
+
+  <!-- Conteúdo das páginas -->
+  <div class="relative top-[88px]">
+    <RouterView />
+  </div>
+
+  <!-- Footer -->
+</template>
---
+++ src/views/home/pages/SupportHomePage.vue
@@ -0,0 +1,8 @@
+
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>Support Page</h1>
+</template>

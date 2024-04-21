3047a5075d176149eadb1276602825a03fdb53e4
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-21 07:52:38 -0300

    Atualiza App.vue para carregar as páginas das rotas

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
 
 <template>
   <div class="font-NunitoSans">
-    <p class="font-bold underline bg-red-300">Página inicial como exemplo</p>
+    <RouterView />
   </div>
 </template>

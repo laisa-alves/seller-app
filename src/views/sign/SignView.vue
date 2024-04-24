5c4eb90cb9f931bab9acd65d818b83fd2754e338
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-24 19:02:29 -0300

    Adiciona roteamento dinâmico para páginas de login e cadastro
    - Melhora a experiência do usuário ao acessar as páginas de login e cadastro
    - Simplifica a lógica de roteamento e renderização das páginas de login e cadastro

---
+++ src/views/sign/SignView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import SigninPage from './pages/SigninPage.vue'
+import SignupPage from './pages/SignupPage.vue'
+
+const route = useRoute()
+
+const currentRoute = route.path.includes('signin') ? SigninPage : SignupPage
+</script>
+
+<template>
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img src="@/assets/images/deliverme.svg" alt="Deliver me" class="mx-auto h-8 w-auto" />
+      <h2
+        class="mt-10 uppercase text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        Portal do Parceiro
+      </h2>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-sm">
+      <component :is="currentRoute"></component>
+    </div>
+  </div>
+</template>

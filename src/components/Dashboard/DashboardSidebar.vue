dc2119544188d4ff1ba940b07bc538261a8d2711
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-26 14:25:57 -0300

    Adiciona componente de sidebar
    O componente é flexível e escalável, permitindo a adição de grupos de menus e itens de menu conforme necessário.

---
+++ src/components/Dashboard/DashboardSidebar.vue
@@ -0,0 +1,225 @@
+<script setup lang="ts">
+import SidebarItemMenu from './SidebarItemMenu.vue'
+import { ref } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const menuGroups = ref([
+  {
+    name: 'Menu',
+    menuItems: [
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>`,
+        label: 'Dashboard',
+        route: '/dashboard'
+      },
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+            />
+          </svg>`,
+        label: 'Avaliações',
+        route: '/'
+      },
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m9 14.25 6-6m4.5-3.493V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185ZM9.75 9h.008v.008H9.75V9Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008V13.5Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>`,
+        label: 'Pedidos',
+        route: '/'
+      },
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+            />
+          </svg>`,
+        label: 'Cardápio',
+        route: '/'
+      },
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>`,
+        label: 'Horários',
+        route: '/'
+      }
+    ]
+  },
+  {
+    name: 'Ajustes',
+    menuItems: [
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>`,
+        label: 'Profile',
+        route: '/'
+      },
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>`,
+        label: 'Configurações',
+        route: '/'
+      },
+      {
+        icon: `<svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15m3 0 3-3m0 0-3-3m3 3H9"
+            />
+          </svg>`,
+        label: 'Logout',
+        route: '/'
+      }
+    ]
+  }
+])
+</script>
+
+<template>
+  <div class="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
+    <div class="flex items-center justify-center h-14 border-b">
+      <RouterLink :to="{ name: 'dashboard' }">
+        <img src="@/assets/images/deliverme.svg" alt="Deliver me logo" />
+      </RouterLink>
+
+      <!-- Implementar o Nome da loja -->
+    </div>
+    <div class="no-scrollbar overflow-y-auto overflow-x-hidden flex-growr">
+      <nav class="mt-4">
+        <template v-for="menuGroup in menuGroups" :key="menuGroup.name">
+          <h3 class="mb-4 ml-4 text-sm font-medium tracking-wide text-gray-500">
+            {{ menuGroup.name }}
+          </h3>
+
+          <ul class="flex flex-col space-y-1">
+            <SidebarItemMenu
+              v-for="(menuItem, index) in menuGroup.menuItems"
+              :item="menuItem"
+              :key="index"
+              :index="index"
+            />
+          </ul>
+        </template>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<!-- Menu name -->
+<!-- <li class="px-5">
+          <div class="flex flex-row items-center h-8">
+            <div class="text-sm font-light tracking-wide text-gray-500">Menu</div>
+          </div>
+        </li> -->
+<!-- Menu name -->
+
+<!-- Menu list - Dashboard -->
+<!-- <li>
+          <a
+            href="#"
+            class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-8 border-transparent hover:border-indigo-500 pr-6"
+          >
+            <span class="inline-flex justify-y-center items-center ml-4"
+              ><HomeIcon class="w-5 h-5"></HomeIcon
+            ></span>
+            <span class="ml-2 text-sm tracking-wide truncate">Dashboard</span>
+          </a>
+        </li> -->
+<!-- Menu list -->
+
+<!-- <li class="px-5">
+          <div class="flex flex-row items-center h-8">
+            <div class="text-sm font-light tracking-wide text-gray-500">Ajustes</div>
+          </div>
+        </li> -->

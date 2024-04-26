19af79d5de134815e84d6b9c5def4aa96bd83fa2
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-04-26 14:21:05 -0300

    Cria a view de Dashboard e importa os componentes

---
+++ src/views/Dashboard/DashboardView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import DashboardSidebar from '@/components/Dashboard/DashboardSidebar.vue'
+</script>
+
+<template>
+  <div
+    class="min-h-screen ml-64 flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-100 text-gray-800"
+  >
+    <div>Essa é a View de dashboard</div>
+  </div>
+  <DashboardSidebar></DashboardSidebar>
+</template>

0217182489fa9cd5903a505a6c7f570f75b99d94
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-03 15:29:22 -0300

    Adiciona estado com pinea para o sidebar

---
+++ src/stores/sidebar.ts
@@ -0,0 +1,14 @@
+import { useStorage } from '@vueuse/core'
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useSidebarStore = defineStore('sidebar', () => {
+  const isSidebarOpen = ref(false)
+  const page = useStorage('page', ref('Dashboard'))
+
+  function toggleSidebar() {
+    isSidebarOpen.value = !isSidebarOpen.value
+  }
+
+  return { isSidebarOpen, toggleSidebar, page }
+})

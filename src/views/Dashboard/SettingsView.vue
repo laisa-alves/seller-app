4f1fb58bc6fa1cec933c8775dbf085624e2f4e79
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-07 10:24:36 -0300

    Cria estrutura básica das páginas do menu de dashboard

---
+++ src/views/Dashboard/SettingsView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PageTitle from '@/components/Dashboard/PageTitle.vue'
+import { ref } from 'vue'
+
+const pageTitle = ref('Configurações')
+</script>
+
+<template>
+  <div class="mx-auto max-w-7xl">
+    <PageTitle :pageTitle="pageTitle" />
+  </div>
+</template>

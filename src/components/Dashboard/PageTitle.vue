d8ccec0457fa33a6a305e3be0af4c8ac90955b39
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-07 08:59:20 -0300

    Adiciona componente de título da página

---
+++ src/components/Dashboard/PageTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps(['pageTitle'])
+</script>
+
+
+<template>
+  <div class="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+    <h2 class="text-2xl font-semibold text-black">
+      {{ props.pageTitle }}
+    </h2>
+  </div>
+</template>
+

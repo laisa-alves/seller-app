b6785fb09caeca8a5f54210b300b946bdef75a73
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-09 12:12:13 -0300

    Cria a estrutura inicial para adicionar os produtos

--- src/views/Dashboard/MenuView.vue
+++ src/views/Dashboard/MenuView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import PageTitle from '@/components/Dashboard/PageTitle.vue'
+import CardComponent from '@/components/Dashboard/Card/CardComponent.vue'
+
 import { ref } from 'vue'
 
 const pageTitle = ref('Cardápio')
@@ -8,5 +10,9 @@ const pageTitle = ref('Cardápio')
 <template>
   <div class="mx-auto max-w-7xl">
     <PageTitle :pageTitle="pageTitle" />
+
+    <CardComponent>
+      <p>Aqui vai ficar o cardápio</p>
+    </CardComponent>
   </div>
 </template>

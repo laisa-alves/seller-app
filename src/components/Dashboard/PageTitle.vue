92cd5ae9509f1aac76573328c4f215ef43967f04
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-09 12:10:37 -0300

    Refatora o título para h1

--- src/components/Dashboard/PageTitle.vue
+++ src/components/Dashboard/PageTitle.vue
@@ -5,9 +5,9 @@ const props = defineProps(['pageTitle'])
 
 <template>
   <div class="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-    <h2 class="text-2xl font-semibold text-black">
+    <h1 class="text-2xl font-semibold text-black">
       {{ props.pageTitle }}
-    </h2>
+    </h1>
   </div>
 </template>
 

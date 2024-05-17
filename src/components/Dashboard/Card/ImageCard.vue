1b068f43f03a7edf2f4cdebc4ceaaf5d1334a935
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-17 07:29:45 -0300

    Cria um componente para as imagens de perfil da loja

---
+++ src/components/Dashboard/Card/ImageCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    default: '',
+    required: true
+  },
+  h: {
+    type: String,
+    default: 'h-14'
+  },
+  w: {
+    type: String,
+    default: 'w-14'
+  }
+})
+</script>
+
+<template>
+  <div :class="`${h} ${w} rounded-full bg-white overflow-hidden`">
+    <img :src="src" alt="Logo" class="w-full h-full object-cover object-center" />
+  </div>
+</template>

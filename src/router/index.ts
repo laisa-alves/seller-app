7b99e87acbb31eb418ac7a7826f987962854f61a
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-20 20:08:07 -0300

    Corrige tipos e adiciona rota de criar  loja

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteLocation } from 'vue-router'
 import HomeView from '@/views/Home/HomeView.vue'
 import InitialHomePage from '@/views/Home/Pages/InitialHomePage.vue'
 import AboutHomePage from '@/views/Home/Pages/AboutHomePage.vue'
@@ -6,20 +6,20 @@ import SupportHomePage from '@/views/Home/Pages/SupportHomePage.vue'
 import BlogHomePage from '@/views/Home/Pages/BlogHomePage.vue'
 import SignView from '@/views/Sign/SignView.vue'
 import DashboardView from '@/views/Dashboard/DashboardView.vue'
-import { Auth } from '../auth.ts'
-import ProfileView from '@/views/Dashboard/ProfileView.vue'
+import { Auth } from '../auth'
+import MyShopsView from '@/views/Dashboard/myShopsView.vue'
 import DashboardLayout from '@/layouts/DashboardLayout.vue'
 import RaitingsView from '@/views/Dashboard/RaitingsView.vue'
 import OrdersView from '@/views/Dashboard/OrdersView.vue'
 import MenuView from '@/views/Dashboard/MenuView.vue'
 import SettingsView from '@/views/Dashboard/SettingsView.vue'
+import shopFormView from '@/views/Dashboard/shopFormView.vue'
 import SignStore from '@/views/Sign/SignStore.vue'
-import path from 'path'
 
 const auth = new Auth()
 
 // Bloqueia a rota para usuários não autenticados e redireciona para para login
-const authGuard = (to: RouteLocation, from: RouteLocation, next: RouteLocation) => {
+const authGuard = (to: RouteLocation, from: RouteLocation, next: Function) => {
   if (auth.isLoggedIn()) {
     next()
   } else {
@@ -28,7 +28,7 @@ const authGuard = (to: RouteLocation, from: RouteLocation, next: RouteLocation) 
 }
 
 // Bloqueia a rota para usuários autenticados e redireciona para dashboard
-const guest = (to: RouteLocation, from: RouteLocation, next: RouteLocation) => {
+const guest = (to: RouteLocation, from: RouteLocation, next: Function) => {
   if (auth.isLoggedIn()) {
     next('/dashboard')
   } else {
@@ -109,15 +109,20 @@ const router = createRouter({
           component: MenuView
         },
         {
-          path: 'profile',
-          name: 'profile',
-          component: ProfileView,
+          path: 'shops',
+          name: 'shops',
+          component: MyShopsView,
           children: [
             {
               path: ':id/edit',
               name: 'storeEdit',
-              component: ProfileView,
+              component: shopFormView,
               props: true
+            },
+            {
+              path: 'new',
+              name: 'storeNew',
+              component: shopFormView
             }
           ]
         },

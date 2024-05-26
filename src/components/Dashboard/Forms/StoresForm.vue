afb2fc45ac7c0011341ada08f07506b05649ea29
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-26 18:31:24 -0300

    Adiciona um link para retorno para página anterior

--- src/components/Dashboard/Forms/StoresForm.vue
+++ src/components/Dashboard/Forms/StoresForm.vue
@@ -29,9 +29,9 @@ const computedSrc = computed(() => {
 
 // Create shop interface
 interface Shop {
-  id: number;
-  name: string;
-  image_url: string;
+  id: number
+  name: string
+  image_url: string
 }
 
 // Check route params
@@ -57,8 +57,8 @@ const handleChange = (event: Event) => {
 
 // Create values interface
 interface Values {
-  name: string;
-  image: { file: any }[];
+  name: string
+  image: { file: any }[]
 }
 
 // Send request
@@ -88,6 +88,23 @@ const handleSubmit = async (values: Values) => {
 </script>
 
 <template>
+  <!-- Return link -->
+  <RouterLink :to="{ name: 'shops' }">
+    <div class="flex gap-1 items-center text-deep-orange-600 mb-8">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="size-5"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+      </svg>
+      voltar
+    </div>
+  </RouterLink>
+
   <!-- Edição das informações -->
   <div class="flex flex-col">
     <!-- Edição da Logo -->

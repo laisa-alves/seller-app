ccb58063fdc2152b8e49d8589e982c1efd133c92
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-05-28 07:43:47 -0300

    Permite lidar com rotas aninhadas ao fazer requisições para api

--- src/services/api.ts
+++ src/services/api.ts
@@ -10,8 +10,8 @@ class BaseApiService {
     this.resource = resource
   }
 
-  getUrl(id = '') {
-    return `${this.baseUrl}${this.resource}/${id}`
+  getUrl(...segments: (string | number)[]) {
+    return `${this.baseUrl}${this.resource}/${segments.join('/')}`
   }
 
   handleErrors(err: Error): void {
@@ -19,9 +19,14 @@ class BaseApiService {
     throw err
   }
 
-  async request(method: string, id = '', data: undefined | object, headers = {}): Promise<any> {
+  async request(
+    method: string,
+    segments: (string | number)[] = [],
+    data: undefined | object = undefined,
+    headers: object = {}
+  ): Promise<any> {
     try {
-      const response = await fetch(this.getUrl(id), {
+      const response = await fetch(this.getUrl(...segments), {
         method,
         headers: {
           Accept: 'application/json',
@@ -41,9 +46,6 @@ class BaseApiService {
       auth.signOut()
       location.reload()
     }
-    // if (response.status === 204) {
-    //   location.reload()
-    // }
   }
 }
 
@@ -53,16 +55,16 @@ class ModelApiService extends BaseApiService {
   }
 
   async get(id = '', headers: object): Promise<any> {
-    return this.request('GET', id, undefined, headers)
+    return this.request('GET', [id], undefined, headers)
   }
   async post(data: object, headers: object): Promise<any> {
-    return this.request('POST', '', data, headers)
+    return this.request('POST', [], data, headers)
   }
   async put(id: string, data: object, headers: object): Promise<any> {
-    return this.request('PUT', id, data, headers)
+    return this.request('PUT', [id], data, headers)
   }
   async delete(id: string, headers: object): Promise<void> {
-    return this.request('DELETE', id, undefined, headers)
+    return this.request('DELETE', [id], undefined, headers)
   }
 }
 
@@ -72,6 +74,39 @@ class StoresApiService extends ModelApiService {
   }
 }
 
+// Nested routes
+class ProductsApiService extends BaseApiService {
+  constructor() {
+    super('stores')
+  }
+
+  async getAllProducts(storeId: string, headers: object): Promise<any> {
+    return this.request('GET', [storeId, 'products'], undefined, headers)
+  }
+
+  async getProduct(storeId: string, productId: string, headers: object): Promise<any> {
+    return this.request('GET', [storeId, 'products', productId], undefined, headers)
+  }
+
+  async createProduct(storeId: string, data: object, headers: object): Promise<any> {
+    return this.request('POST', [storeId, 'products'], data, headers)
+  }
+
+  async updateProduct(
+    storeId: string,
+    productId: string,
+    data: object,
+    headers: object
+  ): Promise<any> {
+    return this.request('PUT', [storeId, 'products', productId], data, headers)
+  }
+
+  async deleteProduct(storeId: string, productId: string, headers: object): Promise<void> {
+    return this.request('DELETE', [storeId, 'products', productId], undefined, headers)
+  }
+}
+
 export const $api = {
-  stores: new StoresApiService()
+  stores: new StoresApiService(),
+  products: new ProductsApiService()
 }

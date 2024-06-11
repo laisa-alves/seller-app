7bfd62ecf7e5f862a39c652611bec0143b548e30
Laísa Alves <124175402+laisa-alves@users.noreply.github.com>
2024-06-11 11:05:44 -0300

    Adiciona as actions para criar e atualizar produto

--- src/stores/productsStore.js
+++ src/stores/productsStore.js
@@ -11,7 +11,7 @@ export const useProductStore = defineStore('productStore', {
     isLoading: false
   }),
   actions: {
-    // Get products from api
+    // Get all products from api
     async fetchProductsFromAPI() {
       const shopStore = useShopStore()
       const mainShopId = shopStore.mainShopId
@@ -33,6 +33,68 @@ export const useProductStore = defineStore('productStore', {
         this.products = response.result.products
       } catch (err) {
         console.log(err)
+      } finally {
+        this.isLoading = false
+      }
+    },
+
+    // Create new product
+    async createProduct(product) {
+      const shopStore = useShopStore()
+      const mainShopId = shopStore.mainShopId
+      try {
+        this.isLoading = true
+
+        const formData = new FormData()
+        formData.append('product[title]', product.title)
+        formData.append('product[price]', product.price)
+        formData.append('product[category]', product.category)
+        formData.append('product[description]', product.description)
+        formData.append('product[active', product.active)
+
+        if (product.image) {
+          formData.append('product[image]', product.image)
+        }
+
+        const headers = {
+          Authorization: `Bearer ${token}`
+        }
+
+        const response = await $api.products.postProduct(mainShopId, formData, headers)
+        this.products.push(response)
+      } catch (err) {
+        console.error(err)
+      } finally {
+        this.isLoading = false
+      }
+    },
+
+    // Edit product
+    async updateProduct(product) {
+      const shopStore = useShopStore()
+      const mainShopId = shopStore.mainShopId
+      try {
+        this.isLoading = true
+
+        const formData = new FormData()
+        formData.append('product[title]', product.title)
+        formData.append('product[price]', product.price)
+        formData.append('product[category]', product.category)
+        formData.append('product[description]', product.description || '')
+        formData.append('product[active', product.active)
+
+        if (product.image) {
+          formData.append('product[image]', product.image)
+        }
+
+        const headers = {
+          Authorization: `Bearer ${token}`
+        }
+
+        const response = await $api.products.putProduct(mainShopId, product.id, formData, headers)
+        this.products = this.products.map((p) => (p.id === product.id ? response : p))
+      } catch (err) {
+        console.error(err)
       } finally {
         this.isLoading = false
       }
